145bae363ef9422ecca804cbf84fb7e95321c567
HussainSalah404 <hussainsalah404@gmail.com>
2025-08-31 20:32:53 +0300

    add search functionality to BookAdmin for enhanced filtering

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,6 +3,7 @@ from .models import Book
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'author', 'published_date')
     list_filter = ('title', 'author', 'published_date')
     publication_year = ('published_date')
 
