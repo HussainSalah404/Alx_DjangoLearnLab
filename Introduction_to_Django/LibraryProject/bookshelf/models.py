335f5f7d178113eeadb2998f677ec1c1e81b253c
HussainSalah404 <hussainsalah404@gmail.com>
2025-08-31 17:03:14 +0300

    add Book model with attributes for title, author, and published date

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    """
+Represents a book in the system.
+
+Attributes:
+    title (str): The title of the book. 
+                    Limited to 200 characters.
+    author (str): The author of the book. 
+                    Limited to 100 characters.
+    published_date (int): The year the book was published.
+                            Stored as an integer.
+"""
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.IntegerField()
