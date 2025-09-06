3520773474d7f90e2ddcec6d534986e9a04f1d17
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-06 05:46:50 +0300

    add: import Librarian model to query library information in query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,8 @@
-from relationship_app.models import name, Library, Author, Book
+from relationship_app.models import name, Library, Author, Book, Librarian
 
 books = Library.objects.get(name=library_name)
 books.all()
 
 author = Author.objects.get(name=author_name)
 books = Book.objects.filter(author=author)
+library = Librarian.objects.get(library=Librarian)
