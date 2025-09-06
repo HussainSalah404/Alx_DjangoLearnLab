5b33b105ac6c0b6c737c733e3f85218758264a99
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-06 17:55:33 +0300

    add: implement book list view and templates for displaying books and library details

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from relationship_app.models import Book
 
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to my book shelf.")
+
+def book_list(request):
+    """Retrieves all books and renders a template displaying the list."""
+    books = Book.objects.all()  # Fetch all book instances from the database
+    context = {'book_list': books}  # Create a context dictionary with book list
+    return render(request, 'relationship_app/list_books.html', context)
+
