8eefbcda94cbb76afa4ca621efd2955ad1303c18
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-06 18:33:21 +0300

    fix: update URL pattern for book list view and import Library model in views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.book_list, name="book_list")
+    path("books/", views.book_list, name="book_list")
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
+from django.views.generic import DetailView
 from django.shortcuts import render, HttpResponse
-from relationship_app.models import Book
+from relationship_app.models import Book, Library
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,12 @@ def book_list(request):
     context = {'book_list': books}  # Create a context dictionary with book list
     return render(request, 'relationship_app/list_books.html', context)
 
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books"] = self.object.books.all()
+        return context
