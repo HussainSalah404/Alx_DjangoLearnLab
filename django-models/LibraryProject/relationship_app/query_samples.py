4cb61e39fe937ddfe6bf5bd479e7814bc4ef34b0
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-06 05:41:35 +0300

    add: import Author and Book models; retrieve books by author in query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,4 +1,7 @@
-from relationship_app.models import name, Library
+from relationship_app.models import name, Library, Author, Book
 
 books = Library.objects.get(name=library_name)
 books.all()
+
+author = Author.objects.get(name=author_name)
+books = Book.objects.filter(author=author)
