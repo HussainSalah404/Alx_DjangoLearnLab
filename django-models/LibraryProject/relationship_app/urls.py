27780e371ed241b76f6c776de82eeb4cce46d72e
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-07 03:52:02 +0300

    fix: correct class name for register view in views.py and update import in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from views import Register
+from .views import register
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -10,5 +10,5 @@ urlpatterns = [
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library-detail"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-    path('register/', Register.as_view(), name='register'),
+    path('register/', register.as_view(), name='register'),
 ]
