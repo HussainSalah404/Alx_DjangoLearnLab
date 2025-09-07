86417bc3e52a97bfd05e542996c1b2bb559e5509
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-07 22:54:36 +0300

    ...

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -35,7 +35,7 @@ class LibraryDetailView(DetailView):
 
 class register(CreateView):
     """Handles user registration using Django's built-in UserCreationForm."""
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy("login")
     template_name = "relationship_app/register.html"
 
