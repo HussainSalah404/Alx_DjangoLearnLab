afbef9c0ff256fa96f768516be8b4b8321c4c570
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-07 03:39:07 +0300

    feat: add user authentication views and templates for login, logout, and registration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from . import views
 from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("books/", views.list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library-detail")
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library-detail"),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,10 @@
 from django.views.generic.detail import DetailView
-from django.shortcuts import render, HttpResponse
+from django.views.generic import CreateView
 from .models import Library, Book
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -26,3 +30,13 @@ class LibraryDetailView(DetailView):
         # Add all related books to the same variable name used in list_books
         context["list_books"] = self.object.books.all()
         return context
+    
+class Register(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
+
+@login_required
+def profile_view(request):
+    # This view can only be accessed by authenticated users
+    return render(request, 'relationship_app/login.html')
