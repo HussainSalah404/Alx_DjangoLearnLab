58f54a5c2b34c42f110a2e5a1400cc079c95181e
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-07 03:44:12 +0300

    fix: specify template for logout view in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("books/", views.list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library-detail"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout')
 ]
