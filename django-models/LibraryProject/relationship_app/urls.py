3cb31220f0c86f1177f8bd42992a75194326df86
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-07 03:58:32 +0300

    fix: specify template for register view in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from .views import register
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -10,5 +9,5 @@ urlpatterns = [
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library-detail"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-    path('register/', register.as_view(template_name='registration/register.html'), name='register'),
+    path('register/', views.register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,6 @@ from .models import Library, Book
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.shortcuts import render, HttpResponse
-#from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 # Create your views here.
 
@@ -35,8 +34,3 @@ class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
-
-"""@login_required
-def profile_view(request):
-    # This view can only be accessed by authenticated users
-    return render(request, 'relationship_app/login.html')"""
