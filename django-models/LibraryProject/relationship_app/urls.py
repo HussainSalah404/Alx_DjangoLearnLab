31ca701c66f899d10ca21ff244dfd607c8afd5a3
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-07 03:47:16 +0300

    fix: add registration path to urls.py for user registration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from views import Register
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("books/", views.list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library-detail"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout')
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('register/', Register.as_view(), name='register'),
 ]
