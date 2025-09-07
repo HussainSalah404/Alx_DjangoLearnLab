84499b3ebae1d8decb34b4f506e539ba00d21c0a
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-07 21:55:31 +0300

    ...

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library-detail"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-    path("register/", views.register(), name="register"),
+    path("register/", views.register.as_view(), name="register"),
     path("admin-dashboard/", admin_view.admin_dashboard, name="admin_dashboard"),
     path("librarian-dashboard/", librarian_view.librarian_dashboard, name="librarian_dashboard"),
     path("member-dashboard/", member_view.member_dashboard, name="member_dashboard"),
