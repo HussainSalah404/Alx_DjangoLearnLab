51156eaea4d9377d24acf63db396789a8d6309e8
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-07 22:52:28 +0300

    ...

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,8 +7,7 @@ from django.http import HttpResponse
 from .models import Library, Book
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import CreateView, UpdateView, DeleteView
-
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth import login
 from .models import Book
 
 # --- General Views ---
