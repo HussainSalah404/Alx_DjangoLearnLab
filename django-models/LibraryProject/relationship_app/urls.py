0f05d2580ad84ee37cb9e0846eadc7bda0c9754b
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-07 03:55:40 +0300

    fix: specify template for register view in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library-detail"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-    path('register/', register.as_view(), name='register'),
+    path('register/', register.as_view(template_name='registration/register.html'), name='register'),
 ]
