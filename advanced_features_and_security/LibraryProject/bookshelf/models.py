177fe98e1939f417e26a058ceb94fe66bf91f9c8
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-11 02:50:01 +0300

    feat: add CustomUser model with manager and extra fields

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 
 # Create your models here.
 class Book(models.Model):
@@ -20,12 +21,36 @@ class Book(models.Model):
     
     def __str__(self):
         return f"{self.title} by {self.author} ({self.published_date})"
-    
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username field is required")
+        if not extra_fields.get("date_of_birth"):
+            raise ValueError("The date_of_birth field is required")
+
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(username, email, password, **extra_fields)
+
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
-    profile_photo = models.ImageField(upload_to="", blank=True, null=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", blank=True, null=True)
 
-    
+    objects = CustomUserManager()
     
     def __str__(self):
         return self.username
