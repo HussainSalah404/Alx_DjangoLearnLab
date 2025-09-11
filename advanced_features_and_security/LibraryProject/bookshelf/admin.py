667cb6c50139951c793e4b099169c87a90fc32d8
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-11 03:04:35 +0300

    in admin registered (CustomUser, CustomUserAdmin)

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+class CustomUserAdmin(UserAdmin):
+    # Fields to display in the admin list view
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_superuser")
+
+    # Fields to filter in the right sidebar
+    list_filter = ("is_staff", "is_superuser", "is_active")
+
+    # How the fields are grouped in the edit page
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+    # Fields to show when creating a new user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
@@ -7,4 +25,5 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'published_date')
     publication_year = ('published_date')
 
-admin.site.register(Book, BookAdmin, CustomUser, CustomUserAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
