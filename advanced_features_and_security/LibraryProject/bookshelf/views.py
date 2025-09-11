d1137aa478428e7358709475ea7faf328a1436b0
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-12 01:34:48 +0300

    add: define book_list

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render , HttpResponse
-
+from .models import Book
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to my book shelf.")
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
