74e6c3eff9d4b11429834e9f00ebdbd36b186ebe
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-12 01:36:43 +0300

    add: permission_required for book_list in bookshelf/views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render , HttpResponse
 from .models import Book
+from django.contrib.auth.decorators import permission_required
+
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to my book shelf.")
 
+@permission_required("bookshelf.view_book", raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, "bookshelf/book_list.html", {"books": books})
