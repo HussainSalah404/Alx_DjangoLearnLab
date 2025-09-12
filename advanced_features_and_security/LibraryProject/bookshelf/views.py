6f711a877ac4c9626e55bb37db78a95ee1f9e55b
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-12 05:55:10 +0300

    add : form_example.html and in bookshelf/views.py added example_form_view

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , HttpResponse
 from .models import Book
 from django.contrib.auth.decorators import permission_required
+from .forms import ExampleForm
 
 # Create your views here.
 def index(request):
@@ -10,3 +11,16 @@ def index(request):
 def book_list(request):
     books = Book.objects.all()
     return render(request, "bookshelf/book_list.html", {"books": books})
+
+def example_form_view(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            # Do something with the data (e.g., save or authenticate)
+            return render(request, "form_example.html", {"form": ExampleForm(), "success": True})
+    else:
+        form = ExampleForm()
+
+    return render(request, "form_example.html", {"form": form})
