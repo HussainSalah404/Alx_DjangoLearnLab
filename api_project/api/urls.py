73245dc562b3d35810e284f05a0056f2249d55e7
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-18 17:03:55 +0300

    feat(api): add BookViewSet with router for full CRUD
    - Added BookViewSet (ModelViewSet) in views.py for CRUD support
    - Registered BookViewSet with DefaultRouter in urls.py
    - Moved queryset/serializer_class definition into BookList
    - Added `books_all/` route to expose router-based endpoints

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books-viewset', BookViewSet, basename='book')
 
 urlpatterns = [
+    # Route for the BookList view (ListAPIView)
     path('books/', BookList.as_view(), name='book-list'),   # Maps to the BookList view
+    
+    # Include the router URLs for BookViewSet (all CRUD operations)
+    path('books_all/', include(router.urls)),  # This includes all routes registered with the router
+    
 ]
