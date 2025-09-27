522e48f7db41b148849fe051438ee5d88d5340d4
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-27 18:17:13 +0300

    feat(api): add DRF generic CRUD views for Book model with permissions
    - Implemented BookListView and BookDetailView (open to all users)
    - Implemented BookCreateView, BookUpdateView, and BookDeleteView (restricted to authenticated users)
    - Applied DRF permission classes to enforce read-only for anonymous users and write access for logged-in users

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+# LIST - open to everyone
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # anyone can view
+
+
+# DETAIL - open to everyone
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+# CREATE - only authenticated users
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+# UPDATE - only authenticated users
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+# DELETE - only authenticated users
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
