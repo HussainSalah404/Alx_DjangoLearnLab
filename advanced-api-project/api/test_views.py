fe0745baf22b87a3774d9453890d229d46a399f6
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-28 18:01:43 +0300

    refactor(tests): update book tests to use Author instance and improve data handling

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,40 +1,58 @@
-from django.contrib.auth.models import User
-from rest_framework.test import APIClient
-from rest_framework import status
 from django.urls import reverse
-from django.test import TestCase
-from .models import Book
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Book, Author
 
-class BookAPITestCase(TestCase):
+
+class BookAPITestCase(APITestCase):
     def setUp(self):
-        self.client = APIClient()
-
-        # Create a test user and log in
-        self.user = User.objects.create_user(username="testuser", password="testpass")
-        self.client.login(username="testuser", password="testpass")
-
-        # Create a sample book
-        self.book = Book.objects.create(title="Test Book", author="Author 1", publication_year=2020)
+        # Create an Author instance to use in tests
+        self.author = Author.objects.create(name="Author 1")
+        # Create a Book linked to the author
+        self.book = Book.objects.create(
+            title="Test Book",
+            author=self.author,
+            publication_year=2020
+        )
 
     def test_create_book(self):
         url = reverse("book-create")
-        data = {"title": "New Book", "author": "Author 2", "publication_year": 2021}
+        data = {
+            "title": "New Book",
+            "author": self.author.id,   # use author ID, not name
+            "publication_year": 2021
+        }
         response = self.client.post(url, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["title"], "New Book")
+        self.assertEqual(response.data["author"], self.author.id)
 
     def test_update_book(self):
         url = reverse("book-update")
-        data = {"id": self.book.id, "title": "Updated Title", "author": "Author 1", "publication_year": 2020}
+        data = {
+            "id": self.book.id,
+            "title": "Updated Title",
+            "author": self.author.id,
+            "publication_year": 2020
+        }
         response = self.client.put(url, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["title"], "Updated Title")
 
     def test_delete_book(self):
         url = reverse("book-delete")
         data = {"id": self.book.id}
         response = self.client.delete(url, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Book.objects.filter(id=self.book.id).exists())
 
     def test_read_books(self):
         url = reverse("book-list")
         response = self.client.get(url, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreaterEqual(len(response.data), 1)  # at least one book
+        self.assertEqual(response.data[0]["title"], "Test Book")
