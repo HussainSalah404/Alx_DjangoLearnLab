8e2d2e4c9614373cd30c4e1eb59b0e42414caa13
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-28 18:03:35 +0300

    ...

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,14 +1,18 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth.models import User
 from .models import Book, Author
 
 
 class BookAPITestCase(APITestCase):
-    
     def setUp(self):
+        # Create a user for authentication
+        self.user = User.objects.create_user(username="testuser", password="password123")
+
         # Create an Author instance to use in tests
         self.author = Author.objects.create(name="Author 1")
+
         # Create a Book linked to the author
         self.book = Book.objects.create(
             title="Test Book",
@@ -16,11 +20,17 @@ class BookAPITestCase(APITestCase):
             publication_year=2020
         )
 
+        # Create API client
+        self.client = APIClient()
+
     def test_create_book(self):
+        # Authenticate before making the request
+        self.client.login(username="testuser", password="password123")
+
         url = reverse("book-create")
         data = {
             "title": "New Book",
-            "author": self.author.id,   # use author ID, not name
+            "author": self.author.id,   # must be ID
             "publication_year": 2021
         }
         response = self.client.post(url, data, format="json")
@@ -30,6 +40,8 @@ class BookAPITestCase(APITestCase):
         self.assertEqual(response.data["author"], self.author.id)
 
     def test_update_book(self):
+        self.client.login(username="testuser", password="password123")
+
         url = reverse("book-update")
         data = {
             "id": self.book.id,
@@ -43,6 +55,8 @@ class BookAPITestCase(APITestCase):
         self.assertEqual(response.data["title"], "Updated Title")
 
     def test_delete_book(self):
+        self.client.login(username="testuser", password="password123")
+
         url = reverse("book-delete")
         data = {"id": self.book.id}
         response = self.client.delete(url, data, format="json")
@@ -51,6 +65,7 @@ class BookAPITestCase(APITestCase):
         self.assertFalse(Book.objects.filter(id=self.book.id).exists())
 
     def test_read_books(self):
+        # Reading does NOT require login
         url = reverse("book-list")
         response = self.client.get(url, format="json")
 
