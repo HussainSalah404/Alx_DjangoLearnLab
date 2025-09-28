e7317649eb6074d3c7d941d1a9f969a06ff2d97a
HussainSalah404 <hussainsalah404@gmail.com>
2025-09-28 18:09:33 +0300

    refactor: remove redundant import of DjangoFilterBackend in views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,6 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from .models import Book
 from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-from django_filters import rest_framework as DjangoFilterBackend
 
 # -----------------------------------------------------------------------------
 # LIST VIEW
