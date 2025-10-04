1e8a4636a7a29dfb3b92a2ef31ec455df3081efd
HussainSalah404 <hussainsalah404@gmail.com>
2025-10-04 12:12:03 +0300

    feat(auth): add user registration and profile views
    - Created `register` view using CustomUserCreationForm with success message
    - Added `profile` view restricted to logged-in users
    - Updated urls.py to include:
      - Django’s built-in authentication URLs
      - routes for registration and profile pages

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("register/", blog_views.register, name="register"),
+    path("profile/", blog_views.profile, name="profile"),
 ]
